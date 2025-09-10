a1848d81d339285b1d709fc81a6bca74faf356bf
rentitforward <rentitforward.app@gmail.com>
2025-09-10 11:26:04 +0800

    add anchor in seller account section

--- src/components/VerificationDashboard.tsx
+++ src/components/VerificationDashboard.tsx
@@ -243,7 +243,7 @@ export const VerificationDashboard = ({ className }: VerificationDashboardProps)
       </Card>
 
       {/* Connect Account Setup */}
-      <Card>
+      <Card id="seller-account">
         <CardHeader>
           <CardTitle className="flex items-center">
             <CreditCard className="w-5 h-5 mr-2" />
